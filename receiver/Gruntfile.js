d05604772c8b8fbe942c9192b9c0010bbfff262a
francesco-strazzullo <me@francescostrazzullo.info>
2014-12-10 21:08:25 +0100

    sender stub

--- receiver/Gruntfile.js
+++ receiver/Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
     wiredep: {
       run: {
        src: [
-          'app/**/*.html'
+          'app/index.html'
         ]
       }
     }
--- sender/Gruntfile.js
+++ sender/Gruntfile.js
@@ -1,13 +1,6 @@
-// Generated on 2014-09-05 using generator-angular 0.9.7
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
@@ -21,16 +14,32 @@ module.exports = function (grunt) {
         options: {
           port: 9000,
           keepalive: true,
-          base:'./app'
+          base: './app'
         }
+      }
+    },
+    wiredep: {
+      run: {
+        src: [
+          'app/index.html'
+        ]
       }
     }
   });
 
-    
-  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {   
-    grunt.task.run([
-      'connect:server'
-    ]);
-  });
+
+  grunt.registerTask('serve', 'Compile then start a connect web server',
+    function(target) {
+      grunt.task.run([
+        'connect:server'
+      ]);
+    });
+
+  grunt.registerTask('run', 'Compile then start a connect web server',
+    function(target) {
+      grunt.task.run([
+        'wiredep',
+        'connect:server'
+      ]);
+    });
 };
